3508cf46ea10b402942bf8de8c81428d6c58b1bc
Utkarsh Kumar <utk.bkkg@gmail.com>
2022-04-25 07:28:41 +0530

    Minor updates

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function Header(props) {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">{props.title}</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,7 +13,7 @@ export default function Header(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                <Link className="nav-link" aria-current="page" to="/">Home</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/about">About</Link>
@@ -29,7 +29,7 @@ export default function Header(props) {
     )
 }
 Header.defaultProps = {
-  title: "Your Title Here",
+  title: "DefaultTitle",
   searchBar: true
 }
 
