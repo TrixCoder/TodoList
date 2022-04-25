db58024b17194f73fe3f50f3a17b7a517856bb57
Utkarsh Kumar <utk.bkkg@gmail.com>
2022-04-25 08:16:30 +0530

    Added dark theme button and dark theme header

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -1,32 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from "react-router-dom";
 
 export default function Header(props) {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">{props.title}</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">About</Link>
-              </li>  
-            </ul>
-            { props.searchBar? <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>: ""}
-          </div>
+  let darkStyle = {
+    navClass: "navbar navbar-expand-lg navbar-dark bg-dark",
+    changeThemeBtn: "btn btn-light" 
+  };
+
+  let lightStyle = {
+    navClass: "navbar navbar-expand-lg navbar-light bg-light",
+    changeThemeBtn: "btn btn-dark" 
+  };
+
+  const [myStyle, setmyStyle] = useState(darkStyle);
+  const [btntxt, setbtntxt] = useState("Light");
+
+  const changeTheme = () => {
+    if (myStyle.navClass === "navbar navbar-expand-lg navbar-dark bg-dark") {
+      setmyStyle({
+        navClass: lightStyle.navClass,
+        changeThemeBtn: lightStyle.changeThemeBtn
+      })
+      setbtntxt("Dark");
+    } else {
+      setmyStyle({
+        navClass: darkStyle.navClass,
+        changeThemeBtn: darkStyle.changeThemeBtn
+      })
+      setbtntxt("Light");
+    }
+  }
+  return (
+    <nav className={myStyle.navClass}>
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">{props.title}</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+          </ul>
+          {props.searchBar ? <form className="d-flex">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form> : ""}
+          <button onClick={changeTheme} type="button" className={myStyle.changeThemeBtn}>{btntxt}</button>
         </div>
-      </nav>
-    )
+      </div>
+    </nav>
+  )
 }
 Header.defaultProps = {
   title: "DefaultTitle",
